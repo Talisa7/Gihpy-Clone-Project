044f3eb7eba8eba69592adcee43a32c185550600
Shelique <81508244+sheshe2795@users.noreply.github.com>
2022-06-05 16:35:21 +0900

    header draft

--- src/Header/header.js
+++ src/Header/header.js
@@ -6,10 +6,13 @@ export default function header() {
     return (
         <div className="head_container">
             <div className="head_box">
-                    <div>
+                <div>
                         <img src={Logo} className="head_logo"/>
-                    </div>
-                    <div className="head_bar">
+                </div>
+                <div className="head_bar">
+
+                    <div className="head_wrap">
+
                     <div className="head_bord">
                         <a href="" className="head_word"> Reactions </a>
                     </div>
@@ -25,11 +28,57 @@ export default function header() {
                     <div className="head_bord">
                     <a href="" className="head_word">Artists</a> 
                     </div>
-                    <div className="head_men">
-                        <a href="" className="head_word"><i class="fas fa-ellipsis-v"></i></a>
-                    </div>
 
                     </div>
+                    
+                    <div className="head_men" id="head_menu">
+                        <a href="" className="head_word"><i class="fas fa-ellipsis-v"></i></a>
+                   
+                        <div className="head_submenu">
+                            <div className="sub_cat">
+                                <p>Categories</p>
+                                <p>GIPHY Studios</p>
+                                <p>Animals</p>
+                                <p>Actions</p>
+                                <p>Anime</p>
+                                <p>Cartoons</p>
+                                <p>Emotions</p>
+                                <p>Food/Drink</p>
+                                <p>Gaming</p>
+                                <p>Holidays/Greetings</p>
+                                <p>Memes</p>
+                                <p>Clips</p>
+                            </div>
+                            <div className="sub_stick">
+                                <p>Stickers</p>
+                                <p>Originals</p>
+                                <p>Trending</p>
+                                <p>Reactions</p>
+                                <p>Packs</p>
+                            </div>
+                            <div className="sub_apps">
+                                <p>APPS</p>
+                                <p>GIPHY</p>
+                                <p>GIPHY World</p>
+                                <p>GIPHY Capture</p>
+                            </div>
+                            <div className="sub_about">
+                                <p>About</p>
+                                <p>Team</p>
+                                <p>Engineering Blog</p>
+                                <p>GIPHY Arts</p>
+                                <p>Studios</p>
+                                <p>Developers</p>                                
+                                <p>Labs</p>                                
+                                <p>FAQ</p>                          
+                                <p>Support</p> 
+                                <p>Jobs</p>
+                                <p>DMCA</p>
+                                <p>Guidlines</p>
+                            </div>                  
+                        </div>
+                    </div>
+            </div>
                     <div className="head_ucbox">
                     <a href="" className="head_upcr">Upload</a>
                     </div>
@@ -42,7 +91,8 @@ export default function header() {
                     </div>
             </div>
             <div className="head_search">
-                <input type="text"></input>
+                <input type="text" className="head_input"></input>
+                <div ><button className="head_serbut"><i className="fas fa-search fa-2x"></i></button></div>
             </div>
 
         </div>
